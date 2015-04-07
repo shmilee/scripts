6a2c68213a12b9375c6cbae19681d220b6976eb9
shmilee <shmilee.zju@gmail.com>
2015-04-07 20:39:50 +0800

    awesome lock screen

--- awesome/new-key.lua
+++ awesome/new-key.lua
@@ -19,5 +19,7 @@
         end),
     --休眠
     awful.key({ modkey, "Control" }, "s", function () awful.util.spawn("systemctl suspend") end),
+    --锁屏
+    awful.key({ modkey, "Control" }, "l", function () awful.util.spawn("slock") end),
     -- 截屏
     awful.key({ }, "Print", function() awful.util.spawn("deepin-scrot") end),

b7eca6e3e28c72b7f6a07084bcebefeb16f41548
shmilee <shmilee.zju@gmail.com>
2015-05-02 13:15:46 +0800

    mode 3

--- th-1a/auto-thvpn.sh
+++ th-1a/auto-thvpn.sh
@@ -2,9 +2,10 @@
 depends=('icedtea-web' 'iproute2' 'sed' 'firefox')
 
 down_file='sslvpn_jnlp.cgi' #change SvpnUid, get by firefox
-mode=2 # 1 2
-_IF=eth0 # valid for mode 2
+mode=3 # 1 2 3
+_IF=eth0 # valid for mode 2 3
 
+myip=$(ip addr show $_IF |sed -n 's/^[ \t].*inet \(.*\)\/.*brd.*$/\1/p')
 if [[ m$mode == m1 ]]; then
     ## local --> '127.0.0.xx TH-1A-LNx', x=1,2,3,8,9
     ## port 2222
@@ -12,8 +13,12 @@ if [[ m$mode == m1 ]]; then
 elif [[ m$mode == m2 ]]; then
     ## local --> '127.0.0.xx TH-1A-LNx', except '$myip TH-1A-LN3'
     ## port 2222
-    myip=$(ip addr show $_IF |sed -n 's/^[ \t].*inet \(.*\)\/.*brd.*$/\1/p')
-    sed -e 's/local=TH-1A-LN[1289]:22/&22/g' -e "s/local=TH-1A-LN3:22/local=$myip:2222/" $down_file >tmp_vpn.jnlp
+    sed -e 's/local=TH-1A-LN[1289]:22/&22/g' -e "s/local=TH-1A-LN3:22/local=$myip:2222/" \
+        $down_file >tmp_vpn.jnlp
+elif [[ m$mode == m3 ]]; then
+    ## local --> '$myip:222x TH-1A-LNx'
+    ## port 2221 2222 2223 2228 2229
+    sed -e "s/local=TH-1A-LN\([12389]\):22/local=$myip:222\1/g" $down_file >tmp_vpn.jnlp
 else
     echo 'Null mode.'
     exit 0

7552e86f90d8084076a70a5358ecf8f87b18ce2a
shmilee <shmilee.zju@gmail.com>
2021-11-02 01:20:36 +0800

    add HLS_Url_Extractor & play encrypted m3u8

--- netinfocap/extractor/hls.py
+++ netinfocap/extractor/hls.py
@@ -4,13 +4,14 @@
 
 import os
 import re
-import requests
 import tempfile
+import contextlib
+import requests
 
 from .extractor import StreamingExtractor
 
 
-REQUESTS_KWARGS = dict(
+REQUESTS_KWS = dict(
     stream=True,
     timeout=10,
     headers={
@@ -21,10 +22,14 @@ REQUESTS_KWARGS = dict(
 
 class HLS_Url_Extractor(StreamingExtractor):
     '''Get HLS uri from Packet.'''
+    display_filter = 'http.request.method==GET'
 
-    def __init__(self, player=None):
+    def __init__(self, player=None, tw=None):
         field_keys = ('fullurl',)
         workers = ('get_http_m3u8_or_key',)
+        if player == 'mpv':
+            player += ' --demuxer-lavf-o-append=allowed_extensions=ts,key'
+            player += ' --demuxer-lavf-o-append=protocol_whitelist=http,tcp,file,crypto,data'
         super(HLS_Url_Extractor, self).__init__(
             field_keys=field_keys, workers=workers, player=player, tw=tw)
         self.tmpdir = os.path.join(tempfile.gettempdir(), 'netinfocap-hls')
@@ -32,53 +37,78 @@ class HLS_Url_Extractor(StreamingExtractor):
             os.mkdir(self.tmpdir)
 
     def download_tmpfile(self, url, fpath=None):
-        suffix = os.path.splitext(url.split('?')[0])[-1]
         if fpath is None:
+            suffix = os.path.splitext(url.split('?')[0])[-1]
             fpath = tempfile.mktemp(prefix='%d-' % self.result['Number'],
                                     suffix=suffix, dir=self.tmpdir)
         try:
-            with closing(requests.get(url, **REQUESTS_KWARGS)) as rp:
-                with open(fpath, "w") as file:
+            with contextlib.closing(requests.get(url, **REQUESTS_KWS)) as rp:
+                with open(fpath, "wb") as file:
                     for data in rp.iter_content(chunk_size=1024):
                         file.write(data)
             return fpath
         except Exception as err:
-            print('\033[31m[Download %s Error]\033[0m' % url, err)
+            self.tw.write(
+                '[Error] Download %s, %s\n' % (url, err), red=True, bold=True)
             return None
 
     def check_need_key(self, m3u8):
+        localm3u8 = self.download_tmpfile(m3u8)
         key = None
-        with open(m3u8, 'r') as file:
-            for i in range(20):  # check 20 lines
-                line = file.readline()
-                if line.startswith('#EXT-X-KEY:'):
-                    m = re.match('''.*URI=["'](?P<key>.*)["'].*''', line)
-                    key = m.groupdict['key']
-        return key
+        if localm3u8:
+            with open(localm3u8, 'r') as file:
+                for i in range(20):  # check 20 lines
+                    line = file.readline()
+                    if line.startswith('#EXT-X-KEY:'):
+                        m = re.match('''.*URI=["'](?P<key>.*)["'].*''', line)
+                        if m:
+                            key = m.groupdict()['key']
+                            break
+        if key:
+            prefix = os.path.splitext(os.path.basename(localm3u8))[0]
+            newkey = '%s-%s' % (prefix, key)
+            # edit localm3u8
+            with open(localm3u8, 'r') as file:
+                datalines = file.readlines()
+            with open(localm3u8, 'w') as file:
+                for line in datalines:
+                    if line.startswith('#EXT-X-KEY:'):
+                        # edit key name
+                        file.write(line.replace(key, newkey))
+                    else:
+                        if (line.startswith('#') or line.startswith('http')
+                                or re.match('\s*\n', line)):
+                            file.write(line)
+                        else:
+                            # relative path -> URL
+                            file.write(os.path.dirname(m3u8))
+                            file.write('/' + line)
+            self.result['localm3u8'] = localm3u8
+            # need key
+            self.field_keys = ('fullurl', 'key')
+            # mark localkey file
+            self.result['localkey'] = os.path.join(
+                os.path.dirname(localm3u8), newkey)
 
     def get_http_m3u8_or_key(self, packet):
         '''Get full uri from packet'''
         try:
-            if packet.http.request.method == 'GET':
+            if packet.http.request_method == 'GET':
                 fullurl = packet.http.request_full_uri
-                if fullurl.split('?')[0].endswith('.m3u8'):
+                path = fullurl.split('?')[0]
+                if path.endswith('.m3u8'):
+                    self.result['id'] = '/'.join(path.split('/')[3:-1])
                     self.result['fullurl'] = fullurl
-                    # TODO -> mv code to check_need_key
-                    localm3u8 = self.download_tmpfile(fullurl)
-                    if localm3u8:
-                        key = self.check_need_key(localm3u8)
-                        if key:
-                            self.field_keys = ('fullurl', 'key')
-                elif packet.http.request_full_uri.endswith('.key'):
-                    return 'key', packet.http.request_full_uri
+                    self.check_need_key(fullurl)
+                elif path.endswith('.key'):
+                    # after check_need_key
+                    if 'localkey' in self.result:
+                        localkey = self.download_tmpfile(
+                            fullurl, fpath=self.result['localkey'])
+                        if localkey:
+                            self.result['key'] = fullurl
         except Exception:
             pass
 
-    @staticmethod
-    def result_callback(result):
-        '''set id'''
-        if 'fulluri' in result:
-            path = result['fulluri'].split('?')[0]
-            result['id'] = '/'.join(path.split('/')[-2:])
-            os.remove()
-            return True
+    def play(self):
+        super(HLS_Url_Extractor, self).play(urlkey='localm3u8')

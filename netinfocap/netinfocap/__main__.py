29b2cafe08cf3531d1744932de51f71d7538fd7c
shmilee <shmilee.zju@gmail.com>
2021-11-11 23:56:33 +0800

    netinfocap: mv main(); save results to multiprocessing list

--- netinfocap/netinfocap/__main__.py
+++ netinfocap/netinfocap/__main__.py
@@ -2,7 +2,60 @@
 
 # Copyright (c) 2021 shmilee
 
-from .infocapture import main
+import sys
+import argparse
+
+from .extractor import all_extractors
+from .infocapture import InfoCapture
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Netinfo Capture v0.1 by shmilee",
+        add_help=False,
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('extractor', nargs='*', default='all',
+                        choices=['all'] + all_extractors,
+                        help='Extractors to use (default: all)\n')
+    parser.add_argument('-i', dest='interface', metavar='<interface>',
+                        nargs=1, default='ap0', type=str,
+                        help='name of interface (default: %(default)s)')
+    parser.add_argument('-n', dest='number', metavar='<number>',
+                        nargs=1, default=99, type=int,
+                        help='an amount of results to capture, then stop (default: 99)')
+    parser.add_argument('-p', dest='player', metavar='<player>',
+                        help='Stream extracted URL to a <player>')
+    parser.add_argument('-f', dest='ffmpeg', metavar='<ffmpeg>',
+                        help='<ffmpeg> used to convert streaming media')
+    parser.add_argument('-o', dest='output', metavar='<output>', default='output',
+                        help='save to <output>.json file (default: %(default)s)')
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='Show debug information')
+    parser.add_argument('-h', '--help', action='store_true',
+                        help='Show this help message and exit')
+    args = parser.parse_args()
+    if args.help:
+        parser.print_help()
+        sys.exit()
+
+    if args.extractor == 'all' or 'all' in args.extractor:
+        args.extractor = all_extractors
+    ex_kws = dict(player=args.player, ffmpeg=args.ffmpeg)
+    extractors = {ex: ex_kws for ex in args.extractor}
+    if isinstance(args.number, list):
+        args.number = args.number[0]
+    try:
+        with InfoCapture(extractors, mresult=args.number, debug=args.debug,
+                         interface=args.interface) as infocap:
+            from .server import InfoServer
+            server = InfoServer()
+            while not infocap.collect_isfull():
+                server.start(infocap.Info_Results, port=8000)
+                infocap.collect(output=args.output)
+                server.stop()
+    except Exception as e:
+        print("\n[Main Error] %s!\n" % e)
+
 
 if __name__ == "__main__":
     main()
--- netinfocap/netinfocap/infocapture.py
+++ netinfocap/netinfocap/infocapture.py
@@ -3,10 +3,11 @@
 # Copyright (c) 2021 shmilee
 
 import os
-import sys
 import pyshark
-import argparse
 import json
+import multiprocessing
+
+from .extractor import get_extractor
 
 
 class InfoCapture(pyshark.LiveCapture):
@@ -15,10 +16,28 @@ class InfoCapture(pyshark.LiveCapture):
     Choose infor from each packet and bring them together as a dict.
     '''
 
-    def __init__(self, extractors, **kwargs):
-        # (interface, tshark_path, display_filter, debug, etc.)
+    manager = multiprocessing.Manager()
+
+    def __init__(self, extractors, mresult=999, mrestart=0, **kwargs):
+        '''
+        :param extractors: dict, {name: extractor_kws, ...}
+        :param mresult: max results to capture, then stop.
+        :param mrestart: max restart :meth:`collect`, <=0 -> min(8,mresult)
+        :param kwargs: LiveCapture kwargs
+            (interface, tshark_path, display_filter, debug, etc.)
+        '''
+        self.extractors = []
+        for name in extractors:
+            ex = get_extractor(name, **extractors[name])
+            if ex:
+                self.extractors.append(ex)
+        self.Info_Results = self.manager.list()
+        self.max_result = mresult
+        self.max_restart = mrestart if mrestart > 0 else min(8, mresult)
+        self.restart_count = -1
+        filters = set([ex.display_filter for ex in self.extractors])
+        kwargs['display_filter'] = ' or '.join(filters)
         super(InfoCapture, self).__init__(**kwargs)
-        self.extractors = extractors
         for ex in self.extractors:
             ex.reset()  # reset extractor to initial state
         tw = self.extractors[0].tw
@@ -31,15 +50,24 @@ class InfoCapture(pyshark.LiveCapture):
             tw.write("Using extractor: %s" % ex.intro)
             tw.write(os.linesep)
 
-    def collect(self, result_count, show=True):
+    def collect_isfull(self):
+        if (len(self.Info_Results) >= self.max_result
+                or self.restart_count >= self.max_restart):
+            return True
+        else:
+            return False
+
+    def collect(self, output=None):
         '''
-        :param result_count: an amount of results to capture, then stop.
-        :param show: show each result dict in terminal.
+        :param output: save results to output.json file
         '''
         if len(self.extractors) == 0:
             self._log.critical('No extractors!')
             return
-        results = []
+        tw = self.extractors[0].tw
+        self.restart_count += 1
+        start = 'Restart' if self.restart_count > 0 else 'Start'
+        tw.write(os.linesep + "[Info] %s collecting ..." % start + os.linesep)
         try:
             for packet in self.sniff_continuously(packet_count=None):
                 number = int(packet.number.get_default_value())
@@ -48,10 +76,9 @@ class InfoCapture(pyshark.LiveCapture):
                     try:
                         ex.extract(packet)
                         if ex.complete:  # extractor complete state
-                            results.append(ex.result)
+                            self.Info_Results.append(ex.result)
                             try:
-                                if show:
-                                    ex.pretty_print()
+                                ex.pretty_print()
                                 if ex.player:  # for play streaming
                                     ex.play()
                                 if ex.ffmpeg:  # for save streaming
@@ -63,19 +90,20 @@ class InfoCapture(pyshark.LiveCapture):
                     except Exception as e:
                         self._log.critical("Can't extract info from packet %s"
                                            % number, exc_info=1)
-                if result_count and result_count > 0:
-                    if len(results) >= result_count:
-                        break
+                if len(self.Info_Results) >= self.max_result:
+                    break
         except KeyboardInterrupt:
             self._log.critical("[Interrupt] InfoCapture (collect)!")
-            return results
         except EOFError:
             self._log.critical("[EOFError]!")
-            return results
         except pyshark.capture.capture.TSharkCrashException:
             self._log.critical("[Error] TShark seems to have crashed!")
-            return results
-        return results
+        if output:
+            with open('%s.json' % output, 'w') as out:
+                tw.write(os.linesep)
+                tw.write("[Info] Save results to %s.json ..." % output)
+                json.dump(list(self.Info_Results), out)
+                tw.write(os.linesep)
 
     def _packets_from_tshark_sync(self, packet_count=None, existing_process=None):
         tshark_process = existing_process or self.eventloop.run_until_complete(
@@ -108,72 +136,3 @@ class InfoCapture(pyshark.LiveCapture):
             if tshark_process in self._running_processes:
                 self.eventloop.run_until_complete(
                     self._cleanup_subprocess(tshark_process))
-
-
-allextractors = ['bilive', 'hls', 'rtmpt']
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description="Netinfo Capture v0.1 by shmilee",
-        add_help=False,
-        formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('extractor', nargs='*', default='all',
-                        choices=['all'] + allextractors,
-                        help='Extractors to use (default: all)\n')
-    parser.add_argument('-i', dest='interface', metavar='<interface>',
-                        nargs=1, default='ap0', type=str,
-                        help='name of interface (default: %(default)s)')
-    parser.add_argument('-n', dest='number', metavar='<number>',
-                        nargs=1, default=99, type=int,
-                        help='an amount of results to capture, then stop (default: 99)')
-    parser.add_argument('-p', dest='player', metavar='<player>',
-                        help='Stream extracted URL to a <player>')
-    parser.add_argument('-f', dest='ffmpeg', metavar='<ffmpeg>',
-                        help='<ffmpeg> used to convert streaming media')
-    parser.add_argument('-o', dest='output', metavar='<output>', default='output',
-                        help='save to <output>.json file (default: %(default)s)')
-    parser.add_argument('-d', '--debug', action='store_true',
-                        help='Show debug information')
-    parser.add_argument('-h', '--help', action='store_true',
-                        help='Show this help message and exit')
-    args = parser.parse_args()
-    if args.help:
-        parser.print_help()
-        sys.exit()
-
-    if args.extractor == 'all' or 'all' in args.extractor:
-        args.extractor = allextractors
-    extractors = []
-    for ex in args.extractor:
-        if ex == 'bilive':
-            from .extractor.bilive import Bilive_Url_Extractor
-            extractors.append(Bilive_Url_Extractor(
-                player=args.player, ffmpeg=args.ffmpeg))
-        elif ex == 'hls':
-            from .extractor.hls import HLS_Url_Extractor
-            extractors.append(HLS_Url_Extractor(
-                player=args.player, ffmpeg=args.ffmpeg))
-        elif ex == 'rtmpt':
-            from .extractor.rtmpt import RTMPT_Url_Extractor
-            extractors.append(RTMPT_Url_Extractor(
-                player=args.player, ffmpeg=args.ffmpeg))
-    filters = ' or '.join(set([ex.display_filter for ex in extractors]))
-    if isinstance(args.number, list):
-        args.number = args.number[0]
-    try:
-        res = []
-        interrupt = 0
-        with InfoCapture(
-                tuple(extractors), interface=args.interface,
-                display_filter=filters, debug=args.debug) as infocap:
-            while len(res) < args.number and interrupt < args.number:
-                start = 'Restart' if interrupt > 0 else 'Start'
-                print("\n[Main Info] %s collecting ...\n" % start)
-                res.extend(infocap.collect(args.number-len(res), show=True))
-                interrupt += 1
-    except pyshark.capture.capture.TSharkCrashException:
-        print("\n[Main Error] TShark seems to have crashed!\n")
-    finally:
-        with open('%s.json' % args.output, 'w') as out:
-            json.dump(res, out)

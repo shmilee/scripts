254e144796bbe9de4d8a4ee8098b76815585df58
shmilee <shmilee.zju@gmail.com>
2022-04-20 10:01:59 +0800

    netinfocap v0.9.1

--- netinfocap/setup.py
+++ netinfocap/setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='netinfocap',
-    version='0.8',
+    version='0.9.1',
     description='Netinfo Capture',
     long_description=long_description,
     long_description_content_type='text/x-rst',

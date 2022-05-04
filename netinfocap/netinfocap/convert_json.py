146cb2a0b31562ce62bb9a2e998d735442f8aea5
shmilee <shmilee.zju@gmail.com>
2022-05-04 23:02:17 +0800

    netinfocap, convert *-share.json

--- netinfocap/netinfocap/convert_json.py
+++ netinfocap/netinfocap/convert_json.py
@@ -11,42 +11,12 @@ import tempfile
 from .server import HTML_template, _result2div
 
 
-def add_UniqID(file):
-    '''
-    Add lost control key: 'UniqID', fix: KeyError: 'UniqID'
-    Write new data in *file*.new
-    '''
-    if not os.path.exists(file):
-        print("[Error] Cannot find %s!" % file)
-        return
-    # read
-    with open(file, 'r', encoding='utf8') as fp:
-        out = json.load(fp)
-        results = out['results']
-        L = len(results)
-        print("[Info] Read %d results from '%s' ..." % (L, file))
-        control_keys = out['control_keys']
-    # add
-    for res in results:
-        if 'UniqID' not in res:
-            res['UniqID'] = tempfile.mktemp(prefix='', dir='')
-    if 'UniqID' not in control_keys:
-        control_keys.append('UniqID')
-    # write
-    output = file + '.new'
-    with open(output, 'w', encoding='utf8') as out:
-        print("[Info] Save %d results to %s ..." % (L, output))
-        json.dump(dict(
-            version=1,
-            control_keys=control_keys,
-            results=results,
-        ), out, ensure_ascii=False)
-
-
 def json2html(file, select_filter=('HLS_Url',)):
     '''
     Convert useful info results from *file*.json, *file*.desc to *file*.html
     and create thumbnails if can access url.
+    Create *file*-share.json, netinfocap-share-keys.json
+    and save div content results to share(insert) in other html.
 
     file: str
         file path, like './aaa.json', the desc file './aaa.desc'
@@ -94,7 +64,6 @@ def json2html(file, select_filter=('HLS_Url',)):
     Num = len(results)
     print("[Info] Select %d results to convert ..." % Num)
     outdir = '%s.files' % file
-    outhtml = '%s.html' % file
     if not os.path.exists(outdir):
         os.mkdir(outdir)
     div_res = [_result2div(
@@ -106,8 +75,29 @@ def json2html(file, select_filter=('HLS_Url',)):
     ]
     # outdir relative to html
     div_res = ' '.join(div_res).replace(outdir, os.path.basename(outdir))
+    outhtml = '%s.html' % file
     with open(outhtml, 'w', encoding='utf8') as out:
         out.write(HTML_template % (Num, Num, div_res))
+    # share json with div content results
+    sharejson = '%s-share.json' % file
+    filename = os.path.splitext(os.path.basename(sharejson))[0]
+    with open(sharejson, 'w', encoding='utf8') as out:
+        json.dump(dict(version=1, key=filename, content=div_res,
+                       title='NetInfo Results(%d)' % Num),
+                  out, ensure_ascii=False)
+    print("[Info] Saved %d results." % Num)
+    # update share keys
+    keys_file = os.path.join(
+        os.path.dirname(sharejson), 'netinfocap-share-keys.json')
+    all_keys = []
+    if os.path.exists(keys_file):
+        with open(keys_file, 'r', encoding='utf8') as out:
+            all_keys = json.load(out)
+    if filename not in all_keys:
+        all_keys.append(filename)
+        with open(keys_file, 'w', encoding='utf8') as out:
+            json.dump(sorted(all_keys), out, ensure_ascii=False)
+        print("[Info] Saved '%d +1' keys." % (len(all_keys)-1))
     print("[Info] DONE.")
 
 

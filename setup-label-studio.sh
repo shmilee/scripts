88ba1af3e53f149d3f79b5f381c6bf18aec136fe
shmilee <shmilee.zju@gmail.com>
2024-03-18 15:43:41 +0800

    backup setup-label-studio

---
+++ setup-label-studio.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Copyright (C) 2024 shmilee
+
+studio_path=~/label-studio
+
+# install for ~user
+python3 -m venv "$studio_path"
+source "$studio_path/bin/activate"
+python3 -m pip install label-studio
+echo "export LABEL_STUDIO_DISABLE_SIGNUP_WITHOUT_LINK=true" >> "$studio_path/bin/activate"
+echo "export LABEL_STUDIO_BASE_DATA_DIR=$studio_path/data" >> "$studio_path/bin/activate"
+mkdir "$studio_path/data"
+
+# add localhost usernames
+for i in `seq 1 10`; do
+    who="$(tr -dc a-z </dev/urandom | head -c 5)@localhost"
+    key="pWd+$(cat /proc/sys/kernel/random/uuid | cut -d- -f3)$(tr -dc A-Za-z0-9 </dev/urandom | head -c 8)"
+    echo -e "$i  $who  $key\n" >> "$studio_path/user-info"
+    label-studio start -b --username "$who" --password "$key"
+done
+
+# check
+#label-studio user --username ?name?from?user-info?
+#label-studio start
